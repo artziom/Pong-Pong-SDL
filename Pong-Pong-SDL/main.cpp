7fcb1020df0f9ccbbf685b70b88c60fc771611ca
ArtZiom <artur.ziomek@triverna.pl>
2020-08-12 15:47:54 +0200

    Create graphics window - not finished

--- Pong-Pong-SDL/main.cpp
+++ Pong-Pong-SDL/main.cpp
@@ -1,5 +1,29 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-	std::cout << "Hello World!";
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *args[]) {
+  std::cout << "Hello World!";
+  SDL_Window *window = NULL;
+
+  SDL_Surface *screenSurface = NULL;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\r\n";
+  } else {
+    window = SDL_CreateWindow("Pong-Pong-SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+      std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << "\r\n";
+    } else {
+      screenSurface = SDL_GetWindowSurface(window);
+
+      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    }
+  }
+
+  return 0;
 }
